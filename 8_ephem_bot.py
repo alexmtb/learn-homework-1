d30c296f2fd3e95cd1565891ff53e64e4d945027
Aleksandr <aleximtb@gmail.com>
2025-03-07 22:16:04 +0300

    [ADD] excercise 8 for homework

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -13,7 +13,9 @@
 
 """
 import logging
-
+import settings
+import ephem
+import datetime as dt
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -21,17 +23,18 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log')
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
+# PROXY = {
+#     'proxy_url': 'socks5://t1.learn.python.ru:1080',
+#     'urllib3_proxy_kwargs': {
+#         'username': 'learn',
+#         'password': 'python'
+#     }
+# }
+
 
 
 def greet_user(update, context):
-    text = 'Вызван /start'
+    text = 'Вызван /start\nПривет!'
     print(text)
     update.message.reply_text(text)
 
@@ -39,16 +42,48 @@ def greet_user(update, context):
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
+
+
+def planet_info(update, context):
+    # Get planet name from user input
+    planet_name = update.message.text.split()[1].capitalize()
+    today_date = dt.date.today().strftime('%Y/%m/%d')
+    # dict of planets
+    planets = {
+        'Mercury': ephem.Mercury(),
+        'Venus': ephem.Venus(),
+        'Mars': ephem.Mars(),
+        'Jupiter': ephem.Jupiter(),
+        'Saturn': ephem.Saturn(),
+        'Uranus': ephem.Uranus(),
+        'Neptune': ephem.Neptune(),
+        'Pluto': ephem.Pluto()
+    }
+
+    try:
+        planet = planets[planet_name]
+        planet.compute(today_date)
+
+        # get constellation for planet
+        constellation = ephem.constellation(planet)[1]
+        response = f'Планета {planet_name} сегодня в созвездии {constellation}.'
+    except (KeyError, AttributeError):
+        response = 'Планета не из солнечной системы!'
+        
+    update.message.reply_text(response)
+    
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planet_info))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
+    
+    logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
 
